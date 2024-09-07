e124cb9dde48daac26e43945d9c86048daa5ce44
sevtechcodes <sevimtas0101@gmail.com>
2024-09-07 15:25:30 +0200

    Started to Bracket Validator

--- bracketValidator.js
+++ bracketValidator.js
@@ -1,5 +1,5 @@
 // You're working with an intern that keeps coming to you with JavaScript code that won't run because the braces, brackets, 
-//and parentheses are off. To save you both some time, you decide to write a braces/brackets/parentheses validator.
+// and parentheses are off. To save you both some time, you decide to write a braces/brackets/parentheses validator.
 
 // Let's say:
 
